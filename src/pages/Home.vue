21f73fc84c59ad7157a76c94c3a97fcce6a33177
jijianming <996013797@qq.com>
2022-02-20 14:55:49 +0800

    路由嵌套

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,6 +1,9 @@
 <template>
     <div>
       <h2>首页</h2>
+      <router-link to="/home/message">消息</router-link>
+      <router-link to="/home/shops">商品</router-link>
+      <router-view/>
     </div>
 </template>
 
--- src/router/index.js
+++ src/router/index.js
@@ -8,7 +8,20 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 // 懒加载配置，分包
 const routes = [
     { path: '/', redirect: '/home' },
-    { path: '/home', component: () => import('../pages/Home.vue') },
+    { 
+        path: '/home',
+        component: () => import('../pages/Home.vue'),
+        children: [
+            {
+                path: 'message',
+                component: () => import('../pages/Homemessage.vue')
+            },
+            {
+                path: 'shops',
+                component: () => import('../pages/HomeShops.vue')
+            }
+        ]
+    },
     { path: '/about', component: () => import('../pages/Home.vue') },
     { path: '/user/:username', component: () => import('../pages/User.vue') },
     { path: '/:pathMatch(.*)', component: () => import('../pages/404.vue') },
